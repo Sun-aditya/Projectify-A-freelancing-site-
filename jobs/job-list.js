fea7798e037273d1a2cf791c1d7c10c4f57e9de1
AdityaVikram8 <aditya0247.becse24@chitkara.edu.in>
2025-08-31 23:02:13 +0530

    Added styling for jobs folder

---
+++ jobs/job-list.js
@@ -0,0 +1,29 @@
+const jCategory=[
+    {
+        index:0,
+        image:"../images/google.png",
+        title:"Web Developer",
+        rate:"$900-1200/m",
+        av:"Full Time",
+        companyName:"Google",
+        location:"USA",
+        vacancy:"01",
+        hours:"50h / Week",
+        description: `As a Web Developer at Google, you will design and develop dynamic web applications that
+         scale to millions of users. Responsibilities include creating responsive interfaces, writing clean 
+         and efficient code, optimizing site performance, and ensuring cross-browser compatibility. You will 
+         collaborate closely with product managers, designers, and backend engineers to deliver world-class user
+          experiences.`,
+        workplace:"Work from home",
+        education:"Bachelor degree in computer science Courses",
+        experience:"2 to 5 years",
+
+    },
+    {
+        index:1,
+        image:"../images/uber.png",
+        title:"Buissness Associate",
+        rate:"$900-1200/m",
+        av:"Part Time",
+    },
+]
